0e9512c96c8ea3e524ca7e4f24e03436b439831a
Vishal Gupta <vishalgupta10852@gmail.com>
2025-08-24 23:49:07 +0530

    Solve Question is Srting Compression

---
+++ StringDemo/StringCompression.java
@@ -0,0 +1,42 @@
+package StringDemo;
+
+public class StringCompression {
+    public static String compression(String str) {
+        String newStr = "";
+        // abc
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+
+    public static String sbCompression(String str) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbccdd";
+        // System.out.println(compression(str));
+        System.out.println(sbCompression(str));
+    }
+}
