25b8d721072e4aad03754e06330a8939ae5b17f8
Vishal Gupta <vishalgupta10852@gmail.com>
2025-09-01 23:41:10 +0530

    Learned Constructor and Type

--- Oops/OOPS.java
+++ Oops/OOPS.java
@@ -4,32 +4,57 @@ public class OOPS {
     public static void main(String[] args) {
         Pen p1 = new Pen(); // Created a pen object called p1.
         p1.setColor("blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
+        System.out.println(p1.getTip());
+
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+
+        // BankAccount myAcc = new BankAccount();
+        // myAcc.username = "Vishal";
+        // // myAcc.password = "absbjdhdvd"
+        // myAcc.setPassword("abcdefghj");
     }
 }
 
+// class BankAccount {
+// public String username;
+// private String password;
+
+// public void setPassword(String pwd) {
+// password = pwd;
+// }
+// }
+
 class Pen {
     // Prop + funx
-    String color;
-    int tip;
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
 
     void setColor(String newColor) {
         color = newColor;
     }
 
     void setTip(int newTip) {
-        tip = newTip;
+        this.tip = newTip;
     }
 }
 
-class Students {
-    String name;
-    int age;
-    float percentage; // CGPA
+// class Students {
+// String name;
+// int age;
+// float percentage; // CGPA
 
-    void calcPercentage(int math, int phy, int chem) {
-        percentage = (math + phy + chem) / 3;
-    }
-}
+// void calcPercentage(int math, int phy, int chem) {
+// percentage = (math + phy + chem) / 3;
+// }
+// }
