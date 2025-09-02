676c99776305fe2bdef8da628eeb0ab6c442687d
Vishal Gupta <vishalgupta10852@gmail.com>
2025-09-03 02:24:12 +0530

    Solving few problem

--- Arrays/SecLargeELement.java
+++ Arrays/SecLargeELement.java
@@ -1,33 +1,26 @@
 package Arrays;
 
 public class SecLargeELement {
-    public static void bubbleSort(int arr[]) {
+    public static int bubbleSort(int arr[]) {
+        int largest = -1;
         for (int i = 0; i < arr.length; i++) {
-            boolean Swaped = false;
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    Swaped = true;
-                }
-                if (!Swaped) {
-                    break;
-                }
+            if (arr[i] > largest) {
+                largest = arr[i];
             }
         }
+        int secondLargest = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
     }
 
-    public static void printArr(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("array is already sorted");
-    }
+    // O(2n)
 
     public static void main(String[] args) {
         int arr[] = { 1, 3, 6, 4, 5, 8 };
-        bubbleSort(arr);
-        printArr(arr);
+        System.out.println(bubbleSort(arr));
     }
 }
