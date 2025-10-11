6e5fb2d16a206c6bc4d1b227f27efd9eed46ebb6
Vishal Gupta <vishalgupta10852@gmail.com>
2025-10-11 18:46:24 +0530

    Solved few Problem backtracking

--- Backtracking/Sudoku.java
+++ Backtracking/Sudoku.java
@@ -1,27 +1,24 @@
-
 public class Sudoku {
 
     public static boolean isSafe(int sudoku[][], int row, int col, int digit) {
-
         // column
-        for (int i = 0; i <= 8; i++) {
+        for (int i = 0; i < 9; i++) {
             if (sudoku[i][col] == digit) {
                 return false;
             }
         }
 
         // row
-        for (int j = 0; j <= 8; j++) {
+        for (int j = 0; j < 9; j++) {
             if (sudoku[row][j] == digit) {
                 return false;
             }
         }
 
-        /// Grid
+        // 3x3 grid
         int sr = (row / 3) * 3;
         int sc = (col / 3) * 3;
 
-        // 3X3 matrix single grid
         for (int i = sr; i < sr + 3; i++) {
             for (int j = sc; j < sc + 3; j++) {
                 if (sudoku[i][j] == digit) {
@@ -34,33 +31,37 @@ public class Sudoku {
     }
 
     public static boolean sudokuSolver(int sudoku[][], int row, int col) {
-        // baceCase
+        // ✅ Base case
         if (row == 9) {
             return true;
         }
 
-        // recursion
-
+        // move to next cell
         int nextRow = row, nextCol = col + 1;
         if (col == 8) {
             nextRow = row + 1;
             nextCol = 0;
         }
 
+        // if cell already filled, skip
         if (sudoku[row][col] != 0) {
             return sudokuSolver(sudoku, nextRow, nextCol);
         }
 
+        // try all digits
         for (int digit = 1; digit <= 9; digit++) {
             if (isSafe(sudoku, row, col, digit)) {
                 sudoku[row][col] = digit;
 
-                if (sudokuSolver(sudoku, nextRow, nextCol)) { /// solution Exsits
+                if (sudokuSolver(sudoku, nextRow, nextCol)) {
                     return true;
                 }
+
+                // backtrack
                 sudoku[row][col] = 0;
             }
         }
+
         return false;
     }
 
@@ -73,23 +74,44 @@ public class Sudoku {
         }
     }
 
+    public static boolean isValidSudoku(int sudoku[][]) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                int num = sudoku[row][col];
+                if (num != 0) {
+                    // temporarily remove the number to test validity
+                    sudoku[row][col] = 0;
+                    if (!isSafe(sudoku, row, col, num)) {
+                        return false; // invalid placement
+                    }
+                    sudoku[row][col] = num; // put it back
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int sudoku[][] = {
-                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+                { 0, 5, 7, 6, 0, 0, 0, 4, 0 },
+                { 0, 0, 0, 0, 0, 5, 1, 3, 0 },
+                { 0, 0, 4, 9, 0, 0, 6, 0, 0 },
+                { 5, 0, 0, 3, 7, 6, 0, 0, 0 },
+                { 0, 8, 0, 0, 0, 0, 0, 7, 0 },
+                { 0, 0, 0, 4, 5, 8, 0, 0, 2 },
+                { 0, 0, 1, 0, 0, 7, 9, 0, 0 },
+                { 0, 2, 9, 5, 0, 0, 0, 0, 0 },
+                { 0, 3, 0, 0, 0, 1, 7, 6, 0 }
         };
-        if (sudokuSolver(sudoku, 0, 0)) {
-            System.out.println("solution exsits");
+
+        if (!isValidSudoku(sudoku)) {
+            System.out.println(" Invalid Sudoku input!");
+        } else if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println(" Solution exists!");
             printSudoku(sudoku);
         } else {
-            System.out.println("solution does not exist");
+            System.out.println(" Solution does not exist!");
         }
     }
+
 }
