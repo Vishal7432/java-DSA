205fe014d1a91dfc6b159f96544651ca2e3b122e
Vishal Gupta <vishalgupta10852@gmail.com>
2025-10-14 23:56:55 +0530

    Start ArrayList

---
+++ ArrayList/Practice.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        // ClassName objName = new ClassName()
+        // java Collection Framework
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        // Add Elements
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        // list.add(1, 9);
+
+        System.out.println(list.size());
+
+        // Al size()
+        // print ArrayList
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        // print Array List in reverse
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        // Get Element
+        // int element = list.get(2);
+        // System.out.println(element);
+
+        // // Delete
+        // list.remove(2);
+        // System.out.println(list);
+
+        // Set element at Index
+        // list.set(2, 10);
+        // System.out.println(list);
+
+        // Contain Element
+        // System.out.println(list.contains(5));
+        // System.out.println(list.contains(11));
+
+        // Question : - Find maximum in an Arraylist
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            max = Math.max(max, list.get(i));
+        }
+        System.out.println(max);
+    }
+}
---
+++ ArrayList/Swap.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;;
+
+public class Swap {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+
+        list.set(idx2, temp);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(6);
+        list.add(3);
+        list.add(2);
+        list.add(9);
+        list.add(1);
+
+        // int idx1 = 1, idx2 = 3;
+        // System.out.println(list);
+        // swap(list, idx1, idx2);
+        // System.out.println(list);
+
+        // ArrayList Sort using Collections
+        System.out.println(list);
+        // Collections.sort(list);
+        // System.out.println(list);
+        // 
+
+        // Desending Order sort
+        // Collections.sort(list, Collections.reverseOrder());
+        Collections.reverse(list);
+        System.out.println(list);
+
+    }
+}
