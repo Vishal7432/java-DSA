3ac44ab9d7cddb2c6387232f2f65f87188f14186
Vishal Gupta <vishalgupta10852@gmail.com>
2025-11-10 20:03:43 +0530

    Solved few problem

--- Stack/Practice.java
+++ Stack/Practice.java
@@ -1,30 +1,65 @@
 package Stack;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.Stack;
 
 public class Practice {
 
-    public class stack {
-        static ArrayList<Integer> list = new ArrayList<>();
+    // Stack using via arrayList implementation
+    // public static class Stack {
+    // static ArrayList<Integer> list = new ArrayList<>();
 
-        public static boolean isEmpty() {
-            return list.size() == 0;
-        }
+    // public static boolean isEmpty() {
+    // return list.size() == 0;
+    // }
 
-        // push
-        public static void push(int data) {
-            list.add(data);
-        }
+    // // push
+    // public static void push(int data) {
+    // list.add(data);
+    // }
 
-        // Pop
-        public static int pop() {
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            return top;
-        }
-    }
+    // // Pop
+    // public static int pop() {
+    // if (isEmpty()) {
+    // return -1;
+    // }
+    // int top = list.get(list.size() - 1);
+    // list.remove(list.size() - 1);
+    // return top;
+    // }
+
+    // // Peek
+    // public static int peek() {
+    // if (isEmpty()) {
+    // return -1;
+    // }
+    // return list.get(list.size() - 1);
+    // }
+    // }
 
     public static void main(String[] args) {
+        // Stack s = new Stack();
+
+        // Using java Collrction frameWork
+
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+            // System.out.println(s.pop());
+        }
+
+        // s.push(4);
+        // System.out.println(s.peek());
+
+        // if (!s.isEmpty()) {
+        // System.out.println("Stack is empty!");
+        // }
 
     }
 }
