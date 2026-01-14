f001e78f231cb1f3290d40be360dab27bf4c5ad2
Vishal Gupta <vishalgupta10852@gmail.com>
2026-01-14 21:12:49 +0530

    Solved Few problem

---
+++ Queue/DequeDS.java
@@ -0,0 +1,20 @@
+package Queue;
+
+import java.util.*;
+
+public class DequeDS {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        // deque.removeFirst();
+        System.out.println(deque);
+
+        System.out.println("First el = " + deque.getFirst());
+        System.out.println("Last el = " + deque.getLast());
+
+    }
+}
